9101fd6550e365fa7ad198e24367b0e7ab56f4f1
MadMaxINDIAN <naman22khater@gmail.com>
2022-11-05 17:48:27 +0530

    updated urls

--- client/src/constants/urls.js
+++ client/src/constants/urls.js
@@ -1,2 +1,2 @@
-export const BASE_URL_1 = 'http://localhost:5000';
-export const BASE_URL_2 = 'http://localhost:3000'
+export const BASE_URL_1 = 'https://scrollify-insta.herokuapp.com';
+export const BASE_URL_2 = 'https://scrollify-insta.netlify.app'
